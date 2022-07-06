c08c78b974d1109b7cae4142196e8793436b7baf
David López <david.lopez@iovlabs.org>
2022-07-05 20:44:33 -0500

    Page not found added

---
+++ client/src/views/NotFound.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useRef } from 'react';
+import lottie from 'lottie-web';
+import Header from '../components/Header';
+
+const NotFound = () => {
+
+    const container = useRef(null);
+
+    useEffect(() => {
+        lottie.loadAnimation({
+            container: container.current,
+            renderer: 'svg',
+            loop: true,
+            autoplay: true,
+            animationData: require('../assets/lotties/404.json')
+        })
+    },[])
+
+    // const defaultOptions = {
+    //     loop: true,
+    //     autoplay: true,
+    //     animationData: animationData,
+    //     rendererSettings: {
+    //         preserveAspectRatio: "xMidYMid slice"
+    //     }
+    // };
+    return (
+        <>
+            <Header/>
+            <div className="container" ref={container}></div>
+        </>
+    )
+}
+
+export default NotFound;
